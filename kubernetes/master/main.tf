b49dd6bc864386f54b3a1b39704c304982cfb29f
David Savage <davemssavage@gmail.com>
2016-10-02 20:01:36 +0100

    reinstate deploy_ssh_hosts variable and set type to list

--- kubernetes/master/main.tf
+++ kubernetes/master/main.tf
@@ -1,7 +1,10 @@
 variable "ca_cert_pem" {}
 variable "ca_private_key_pem" {}
-variable "ip_addresses" { type = "list" }
+variable "ip_addresses" {}
 variable "dns_names" { default = "" }
+# supports if you have a public/private ip and you want to set the private ip
+# for internal cert but use the public_ip to connect via ssh
+variable "deploy_ssh_hosts" {}
 variable "master_count" {}
 variable "kube_service_ip" { default = "10.3.0.1" }
 variable "validity_period_hours" { default = "8760" }
